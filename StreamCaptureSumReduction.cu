c429bed246428558d89b409b7c536497b4a3e144
Rohit11544 <brohit11544@gmail.com>
2021-03-09 08:32:55 +0530

    Update StreamCaptureSumReduction.cu

--- StreamCaptureSumReduction.cu
+++ StreamCaptureSumReduction.cu
@@ -1,5 +1,6 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
+#include <helper_cuda.h>
 #include <iostream>
 
 #define SIZE 1024
@@ -53,43 +54,43 @@ void cudaGraphsumReduction(int* h_v, int* h_v_r, int* d_v, int* d_v_r, int TB_SI
 	cudaGraph_t graph;
 	int result = 0;
 
-	cudaStreamCreateWithFlags(&stream1, cudaStreamNonBlocking);
-	cudaStreamCreateWithFlags(&stream2, cudaStreamNonBlocking);
-	cudaStreamCreateWithFlags(&streamForGraph, cudaStreamNonBlocking);
+	checkCudaErrors(cudaStreamCreateWithFlags(&stream1, cudaStreamNonBlocking));
+	checkCudaErrors(cudaStreamCreateWithFlags(&stream2, cudaStreamNonBlocking));
+	checkCudaErrors(cudaStreamCreateWithFlags(&streamForGraph, cudaStreamNonBlocking));
 
-	cudaEventCreate(&forkStreamEvent);
-	cudaEventCreate(&memsetEvent);
+	checkCudaErrors(cudaEventCreate(&forkStreamEvent));
+	checkCudaErrors(cudaEventCreate(&memsetEvent));
 
-	cudaStreamBeginCapture(stream1, cudaStreamCaptureModeGlobal);
-	cudaEventRecord(forkStreamEvent, stream1);
-	cudaStreamWaitEvent(stream2, forkStreamEvent);
+	checkCudaErrors(cudaStreamBeginCapture(stream1, cudaStreamCaptureModeGlobal));
+	checkCudaErrors(cudaEventRecord(forkStreamEvent, stream1));
+	checkCudaErrors(cudaStreamWaitEvent(stream2, forkStreamEvent));
 
-	cudaMemcpyAsync(d_v, h_v, n * sizeof(int), cudaMemcpyDefault, stream1);
+	checkCudaErrors(cudaMemcpyAsync(d_v, h_v, n * sizeof(int), cudaMemcpyDefault, stream1));
 
-	cudaMemsetAsync(d_v_r, 0, n * sizeof(int), stream2);
-	cudaEventRecord(memsetEvent, stream2);
-	cudaStreamWaitEvent(stream1, memsetEvent);
+	checkCudaErrors(cudaMemsetAsync(d_v_r, 0, n * sizeof(int), stream2));
+	checkCudaErrors(cudaEventRecord(memsetEvent, stream2));
+	checkCudaErrors(cudaStreamWaitEvent(stream1, memsetEvent));
 
 	sum_reduction << <GRID_SIZE, TB_SIZE, 0, stream1 >> > (d_v, d_v_r);
 
 	sum_reduction << <1, TB_SIZE, 0, stream1 >> > (d_v_r, d_v_r);
 
-	cudaMemcpyAsync(h_v_r, d_v_r, n * sizeof(int), cudaMemcpyDefault, stream1);
+	checkCudaErrors(cudaMemcpyAsync(h_v_r, d_v_r, n * sizeof(int), cudaMemcpyDefault, stream1));
 
-	cudaStreamEndCapture(stream1, &graph);
+	checkCudaErrors(cudaStreamEndCapture(stream1, &graph));
 
 	cudaGraphNode_t* nodes = NULL;
 	size_t numNodes = 0;
-	cudaGraphGetNodes(graph, nodes, &numNodes);
+	checkCudaErrors(cudaGraphGetNodes(graph, nodes, &numNodes));
 	cout << "\nNum of nodes in the graph created using stream capture API = " << numNodes << endl;
 
 	cudaGraphExec_t graphExec;
-	cudaGraphInstantiate(&graphExec, graph, NULL, NULL, 0);
+	checkCudaErrors(cudaGraphInstantiate(&graphExec, graph, NULL, NULL, 0));
 
 	cudaGraph_t clonedGraph;
 	cudaGraphExec_t clonedGraphExec;
-	cudaGraphClone(&clonedGraph, graph);
-	cudaGraphInstantiate(&clonedGraphExec, clonedGraph, NULL, NULL, 0);
+	checkCudaErrors(cudaGraphClone(&clonedGraph, graph));
+	checkCudaErrors(cudaGraphInstantiate(&clonedGraphExec, clonedGraph, NULL, NULL, 0));
 
 	cudaEvent_t start, stop;
 	cudaEventCreate(&start);
@@ -102,34 +103,34 @@ void cudaGraphsumReduction(int* h_v, int* h_v_r, int* d_v, int* d_v_r, int TB_SI
 		cudaStreamSynchronize(streamForGraph);
 		verify_result(h_v, h_v_r, n);
 	}
-	
+
 	cudaEventRecord(stop);
 	cudaEventSynchronize(stop);
 	float milliseconds = 0;
 	cudaEventElapsedTime(&milliseconds, start, stop);
-	
+
 	cout << "\nVerifying Cloned Graph ... " << endl;
 	for (int i = 0; i < GRAPH_LAUNCH_ITERATIONS; i++)
 	{
-		cudaGraphLaunch(clonedGraphExec, streamForGraph);
-		cudaStreamSynchronize(streamForGraph);
+		checkCudaErrors(cudaGraphLaunch(clonedGraphExec, streamForGraph));
+		checkCudaErrors(cudaStreamSynchronize(streamForGraph));
 		verify_result(h_v, h_v_r, n);
 	}
 	cout << "Done! Verifyied successfully" << endl;
 
 	cout << "\nTime taken by using CUDA GRAPH in ms : " << milliseconds / GRAPH_LAUNCH_ITERATIONS << endl;
 
-	cudaStreamSynchronize(streamForGraph);
+	checkCudaErrors(cudaStreamSynchronize(streamForGraph));
 
-	cudaGraphExecDestroy(graphExec);
-	cudaGraphExecDestroy(clonedGraphExec);
-	cudaGraphDestroy(graph);
-	cudaGraphDestroy(clonedGraph);
-	cudaStreamDestroy(stream1);
-	cudaStreamDestroy(stream2);
-	cudaStreamDestroy(streamForGraph);
-	cudaEventDestroy(start);
-	cudaEventDestroy(stop);
+	checkCudaErrors(cudaGraphExecDestroy(graphExec));
+	checkCudaErrors(cudaGraphExecDestroy(clonedGraphExec));
+	checkCudaErrors(cudaGraphDestroy(graph));
+	checkCudaErrors(cudaGraphDestroy(clonedGraph));
+	checkCudaErrors(cudaStreamDestroy(stream1));
+	checkCudaErrors(cudaStreamDestroy(stream2));
+	checkCudaErrors(cudaStreamDestroy(streamForGraph));
+	checkCudaErrors(cudaEventDestroy(start));
+	checkCudaErrors(cudaEventDestroy(stop));
 }
 
 
@@ -158,8 +159,8 @@ int main() {
 
 	h_v = (int*)malloc(bytes);
 	h_v_r = (int*)malloc(bytes);
-	cudaMalloc(&d_v, bytes);
-	cudaMalloc(&d_v_r, bytes);
+	checkCudaErrors(cudaMalloc(&d_v, bytes));
+	checkCudaErrors(cudaMalloc(&d_v_r, bytes));
 
 	initialize_vector(h_v, n);
 	int	TB_SIZE = SIZE;
@@ -185,15 +186,15 @@ int main() {
 	cout << "\nTime taken without CUDA GRAPH in ms : " << milliseconds / GRAPH_LAUNCH_ITERATIONS << endl;
 
 	cout << "----------------------------------------------------" << endl;
-	
+
 	cout << "Sum Reduction using CUDA GRAPHS (Stream Capture)" << endl;
 	cudaGraphsumReduction(h_v, h_v_r, d_v, d_v_r, TB_SIZE, GRID_SIZE, n);
 
 	cout << "----------------------------------------------------\n" << endl;
 	cout << "\nThe time is the average time of all the kernel launchs. The total kernel launches are " << GRAPH_LAUNCH_ITERATIONS << endl;
 
-	cudaFree(d_v);
-	cudaFree(d_v_r);
+	checkCudaErrors(cudaFree(d_v));
+	checkCudaErrors(cudaFree(d_v_r));
 	return 0;
 }
 
